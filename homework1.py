5d3a906d60e8b9033a446a2318303240f1a1469a
Smajid10 <106848414+Smajid10@users.noreply.github.com>
2022-06-03 18:00:14 -0500

    Update homework1.py

--- homework1.py
+++ homework1.py
@@ -1,6 +1,4 @@
-Python 3.8.3 (v3.8.3:6f8c8320e9, May 13 2020, 16:29:34) 
-[Clang 6.0 (clang-600.0.57)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
+
 >>> from sympy import solve, Symbol, sin, limit, matrix_symbols, diag, plot, zeros
 
 # Question 1
